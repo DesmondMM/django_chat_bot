5ea7cf033a33693dc663821b5839eca3f04024db
desmondmm <desmond@siege.ai>
2017-11-10 19:22:30 +0200

    added some responses

--- yomamabot/fb_yomamabot/views.py
+++ yomamabot/fb_yomamabot/views.py
@@ -15,6 +15,16 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 
 
+jokes = {
+         'stupid': ["""Yo' Mama is so stupid, she needs a recipe to make ice cubes.""",
+                    """Yo' Mama is so stupid, she thinks DNA is the National Dyslexics Association."""],
+         'fat':    ["""Yo' Mama is so fat, when she goes to a restaurant, instead of a menu, she gets an estimate.""",
+                    """ Yo' Mama is so fat, when the cops see her on a street corner, they yell, "Hey you guys, break it up!" """],
+         'dumb':   ["""Yo' Mama is so dumb, when God was giving out brains, she thought they were milkshakes and asked for extra thick.""",
+                    """Yo' Mama is so dumb, she locked her keys inside her motorcycle."""]
+         }
+
+
 class YoMamaBotView(generic.View):
     # The get method is the same as before.. omitted here for brevity
     @method_decorator(csrf_exempt)
@@ -48,7 +58,18 @@ class YoMamaBotView(generic.View):
 
 
 def post_facebook_message(fbid, recevied_message):
-    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token={}'.format(os.getenv('FB_ACCESS_KEY'))
-    response_msg = json.dumps({"recipient": {"id": fbid}, "message": {"text": recevied_message}})
-    status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
+    # Remove all punctuations, lower case the text and split it based on space
+    tokens = re.sub(r"[^a-zA-Z0-9\s]", ' ', recevied_message).lower().split()
+    joke_text = ''
+    for token in tokens:
+        if token in jokes:
+            joke_text = random.choice(jokes[token])
+            break
+    post_message_url = ''
+    if not joke_text:
+        joke_text = "I didn't understand! Send 'stupid', 'fat', 'dumb' for a Yo Mama joke!"
+        post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token={}'.format(
+            os.getenv('FB_ACCESS_KEY'))
+    response_msg = json.dumps({"recipient": {"id": fbid}, "message": {"text": joke_text}})
+    status = requests.post(post_message_url, headers={"Content-Type": "application/json"}, data=response_msg)
     pprint(status.json())
